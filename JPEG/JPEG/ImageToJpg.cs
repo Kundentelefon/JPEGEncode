995d567d19576ba85401637fbe8a1e5bd2bf98eb
Sebastian Schlund <sebastian.o.schlund@gmail.com>
2016-01-18 00:31:17 +0100

    aufteilen

--- JPEG/JPEG/ImageToJpg.cs
+++ JPEG/JPEG/ImageToJpg.cs
@@ -38,6 +38,7 @@ namespace JPEG
             //create JPG head
             PictureHead.CreateJPGHead(bs, (ushort)height, (ushort)width);
 
+            
             //TODO: DCT
             for (int y = 0; y < height; y ++)
             {
@@ -52,6 +53,29 @@ namespace JPEG
             bs.AddShort(0xffd9); //End of Picture Marker
             bs.WriteToFile(imagedest);
             
+
+        }
+
+        public float[][,] aufteilen(float[,]input)
+        {
+            float[][,] returnarry= new float[(input.Length* input.GetLength(0))/64][,];
+            for (int i = 0; i < input.Length; i=i+8)
+            {
+                for (int ia = 0; ia < input.GetLength(0); )
+                {
+                    float[,] temparry = new float[8,8];
+                    for (int ib  = 0; ib < 8; ib++)
+                    {
+                        for (int ic = 0; ic < 8; ic++)
+                        {
+                            temparry[ib, ic] = input[i+ib,ia+ic];
+                        }
+                    }
+                    ia = ia + 8;
+                    returnarry[(i*input.Length+ia*8)/64]= temparry;
+                }
+            }
+            return returnarry;
         }
     }
 }
