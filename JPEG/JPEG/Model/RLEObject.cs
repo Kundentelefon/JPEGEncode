2a75bbf86b2cf76913a2c6cf2d541931059a2373
Martin Eckstein <ow.gaming@gmx.de>
2016-01-18 02:56:11 +0100

    bla

--- JPEG/JPEG/Model/RLEObject.cs
+++ JPEG/JPEG/Model/RLEObject.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace JPEG.Model
 {
-    struct RLEObject
+    class RLEObject
     {
        public List<Bitstream> bitstreams;
-       public List<SortedList<byte, List<bool>>> huffmantablesAC;
-       public List<SortedList<byte, List<bool>>> huffmantablesDC;
+       public List<SortedList<byte, List<bool>>> huffmantablesAC ;
+       public List<SortedList<byte, List<bool>>> huffmantablesDC ;
+       
     }
 }
